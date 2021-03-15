d71517a4d838444761f85431f518e1dfd31d9ac3
Michael Juliano <mjuliano@topquadrant.com>
2021-03-15 14:14:56 -0400

    Added Apache License to new files and cleaned up some leftovers.

--- rdf-delta-server-local/src/main/java/org/seaborne/delta/zk/WrappedUncheckedZkConnection.java
+++ rdf-delta-server-local/src/main/java/org/seaborne/delta/zk/WrappedUncheckedZkConnection.java
@@ -1,3 +1,20 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  See the NOTICE file distributed with this work for additional
+ *  information regarding copyright ownership.
+ */
+
 package org.seaborne.delta.zk;
 
 import org.apache.jena.atlas.json.JsonObject;
@@ -5,6 +22,8 @@ import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.Watcher;
 import java.util.List;
+import java.util.function.Supplier;
+
 import static org.seaborne.delta.server.local.DPS.LOG;
 
 /**
@@ -115,8 +134,13 @@ public final class WrappedUncheckedZkConnection implements UncheckedZkConnection
     }
 
     @Override
-    public ZkLock acquireLock(final String path) {
-        return this.decorated.acquireLock(path);
+    public void runWithLock(String path, Runnable action) {
+        this.decorated.runWithLock(path, action);
+    }
+
+    @Override
+    public <X> X runWithLock(String path, Supplier<X> action) {
+        return this.decorated.runWithLock(path, action);
     }
 
     @Override
--- rdf-delta-server-local/src/main/java/org/seaborne/delta/zk/ZkLockFactory.java
+++
@@ -1,5 +0,0 @@
-package org.seaborne.delta.zk;
-
-public interface ZkLockFactory {
-    ZkLock acquire(String logPath) throws Exception;
-}
